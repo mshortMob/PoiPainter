57b04af5b8c4ac51269b4d1140bbca8d31096ade
Matthew Short <mshort@mobiquityinc.com>
2017-05-03 16:55:31 -0400

    add change orbit button

--- deployToAnimmaperDev.sh
+++ deployToAnimmaperDev.sh
@@ -3,4 +3,4 @@ git add -A
 git commit -a -m "$1"
 git push
 zip Archive.zip *
-aws --profile poipainter s3 sync '/Users/mshort/desktop/PoiPainter' s3://animmappertests
+aws --profile poipainter s3 sync '/Users/mshort/desktop/ead' s3://eat-a-dick
