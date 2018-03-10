c294f56e944d41fdf25fd952111686347ee46656
Matthew Short <mshort@mobiquityinc.com>
2018-03-10 14:13:25 -0500

    remove media inputs

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 rm Archive.zip
 git add -A
-git commit -a -m "$1"
-git push
+#git commit -a -m "$1"
+#git push
 zip Archive.zip *
 aws --profile poipainter s3 sync '/Users/mshort/desktop/PoiPainter' s3://animmappertests
