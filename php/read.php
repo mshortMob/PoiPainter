8522b1a6a158b1084c1f36e145587fb706c5de6a
mshort <mshort@mobiquityinc.com>
2020-07-18 16:15:34 -0400

    overwrite with recent dev work, shader editor

--- php/read.php
+++ php/read.php
@@ -1,5 +1,6 @@
 <?php
     $fileName=$_GET['fn'];
+    $fileName="../".$fileName;
     $myfile = fopen($fileName, "r") or die("Unable to open file!");
     echo fread($myfile,filesize($fileName));
     fclose($myfile);
