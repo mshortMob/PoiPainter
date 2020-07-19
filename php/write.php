00400ce2cefcb161d497f2e6027c28c77f48ce94
mshort <mshort@mobiquityinc.com>
2020-07-19 19:19:39 -0400

    cleaning up

--- php/write.php
+++ php/write.php
@@ -2,6 +2,7 @@
     //$formInput=$_GET['key'];
     $formInput= file_get_contents('php://input');
     $fn=$_GET['fn'];
+    $fn="../".$fn;
     $myfile = fopen($fn, "w") or die("Unable to open file!");
     $txt = $formInput;
     fwrite($myfile, $txt);
